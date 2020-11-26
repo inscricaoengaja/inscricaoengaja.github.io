d9dafafad9021a9e7666d9931a98b38b5c64db5c
Renan Bocchi <67618681+ResXBocchi@users.noreply.github.com>
2020-11-26 16:30:59 -0300

    ajustando mousetrap

--- script.js
+++ script.js
@@ -38,6 +38,17 @@ let funcao = setInterval(function(){
     }
 }, 1000);
 
-$('body').mouseleave(function() {
-    alert('Perdeu a live de ontem? clique em assista agora!');
- });
+var mouse = {
+    x: undefined, 
+    y:undefined
+};
+
+window.addEventListener("mousemove", function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+
+    $('body').mouseleave(function() {
+        if (mouse.y == 0){
+        alert('Perdeu a live? clique em assista agora!');
+    }});
